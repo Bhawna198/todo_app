8d55a83dac531b358fb1d5af5d4aec56b41c4d6a
Bhawna198 <122960027+Bhawna198@users.noreply.github.com>
2025-07-21 14:29:30 +0530

    add element added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TodoList from "./components/TodoList";
+
+function App() {
+  return (
+    <div className="bg-blue-50 min-h-screen flex items-center justify-center p-4">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
